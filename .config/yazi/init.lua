615b1dd599bef4aae4128197ab927e56674adce6
shalom2552 <shalom2552@gmail.com>
2026-09-23 10:03:54 +0300

    feat(yazi): add usefull plugins

--- .config/yazi/init.lua
+++ .config/yazi/init.lua
@@ -8,3 +8,23 @@ th.git.ignored_sign   = "! "
 th.git.clean_sign     = ""
 
 require("git"):setup()
+
+require("full-border"):setup()
+
+require("yatline"):setup({
+	section_separator = { open = "", close = "" },
+	part_separator = { open = "", close = "" },
+	inverse_separator = { open = "", close = "" },
+
+	style_a = {
+		fg = "#1a1b26",
+		bg_mode = {
+			normal = "#7aa2f7",
+			select = "#bb9af7",
+			un_set = "#f7768e",
+		},
+	},
+	style_b = { bg = "#24283b", fg = "#c0caf5" },
+	style_c = { bg = "reset",   fg = "#a9b1d6" }, -- "reset" leaves it transparent
+})
+
