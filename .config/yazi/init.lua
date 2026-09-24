cca0b0cd3d15b4fbc2aec8640ee3219e103327e1
shalom2552 <shalom2552@gmail.com>
2026-09-24 11:43:49 +0300

    feat(yazi): add plugins

--- .config/yazi/init.lua
+++ .config/yazi/init.lua
@@ -1,28 +1,15 @@
-th.git = th.git or {}
-th.git.modified_sign  = "M "
-th.git.added_sign     = "A "
-th.git.deleted_sign   = "D "
-th.git.untracked_sign = "? "
-th.git.updated_sign   = "U "
-th.git.ignored_sign   = "! "
-th.git.clean_sign     = ""
 
 require("git"):setup()
-
 require("full-border"):setup()
 
+-- status lines style (top and bottom)
 require("yatline"):setup({
 	section_separator = { open = "", close = "" },
 	part_separator = { open = "", close = "" },
-	inverse_separator = { open = "", close = "" },
-
+	inverse_separator = { open = "", close = "" },
 	style_a = {
 		fg = "#1a1b26",
-		bg_mode = {
-			normal = "#7aa2f7",
-			select = "#bb9af7",
-			un_set = "#f7768e",
-		},
+		bg_mode = { normal = "#7aa2f7", select = "#bb9af7", un_set = "#f7768e", },
 	},
 	style_b = { bg = "#24283b", fg = "#c0caf5" },
 	style_c = { bg = "reset",   fg = "#a9b1d6" }, -- "reset" leaves it transparent
@@ -32,13 +19,9 @@ require("yatline"):setup({
 -- Check: after `ya pkg upgrade`, `grep -n ':icon(' ~/.config/yazi/plugins/yatline.yazi/main.lua` prints nothing.
 function Yatline.string.get:hovered_file_extension(show_icon)
 	local hovered = cx.active.current.hovered
-	if not hovered then
-		return ""
-	end
+	if not hovered then return "" end
 	local name = hovered.cha.is_dir and "dir" or (hovered.url.name:match("^.+%.(.+)$") or "null")
-	if not show_icon then
-		return name
-	end
+	if not show_icon then return name end
 	local icon = th.icon:match(hovered)
 	return (icon and icon.text or "") .. " " .. name
 end
